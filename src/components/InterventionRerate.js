1fc0cb25a52dd81df8bd17df32cb3ec52b844334
melliemuse <melliemuse@gmail.com>
2020-06-04 15:42:19 -0500

    altered styling, tweaked fetch calls in anxiety rating for interventions and user interventions

--- src/components/InterventionRerate.js
+++ src/components/InterventionRerate.js
@@ -5,7 +5,7 @@ import "./InterventionRerate.css"
 
 export default class InterventionRerate extends Component {
     state = {
-        anxietyScore: "",
+        anxiety_score: "",
         description: "",
         interventionId: ""
     }
@@ -19,27 +19,27 @@ export default class InterventionRerate extends Component {
         let buttons = []
         for (let i = 0; i < 10; i++) {
             buttons.push(
-                <Button className="rerateAnxietyScore" id="anxietyScore" value={i + 1} onClick={this.handleFieldChange} key={i + 1}>{i + 1}</Button>
+                <Button className="rerateAnxietyScore" id="anxiety_score" value={i + 1} onClick={this.handleFieldChange} key={i + 1}>{i + 1}</Button>
             )
         }
         return buttons
     }
     createAnxietyRating = () => {
         console.log(this.props.interventions[this.props.interventions.length - 1].id)
-        if (this.state.anxietyScore === "") {
+        if (this.state.anxiety_score === "") {
             alert("Please select an anxiety score")
         } else {
             const anxiety = {
-                "userId": parseInt(localStorage.getItem("activeUser")),
+                // "userId": parseInt(localStorage.getItem("activeUser")),
                 "interventionId": parseInt(this.props.intervention.id),
-                "timestamp": this.props.interventions[this.props.interventions.length - 1].timestamp,
-                "anxietyScore": parseInt(this.state.anxietyScore),
+                // "timestamp": this.props.interventions[this.props.interventions.length - 1].timestamp,
+                "anxiety_score": parseInt(this.state.anxiety_score),
                 "description": this.state.description,
                 "id": Number(this.props.interventions[this.props.interventions.length - 1].id)
             }
             console.log("anxiety rerate object", anxiety)
-            APIManager.update("userInterventions", anxiety)
-                .then(anxiety.anxietyScore > 3 ? this.props.history.push("/interventions") : this.props.history.push("/charts"))
+            APIManager.update("user_interventions", anxiety)
+                .then(anxiety.anxiety_score > 3 ? this.props.history.push("/interventions") : this.props.history.push("/charts"))
         }
     }
 
@@ -50,6 +50,7 @@ export default class InterventionRerate extends Component {
         return (
             <div className="center">
                 <h2 className="rerate-heading heading center-text">You have completed {this.props.intervention.name.toLowerCase()}!</h2>
+                <div className="box">
                 <h3 className="rerate-subheading heading center-text">How is your anxiety now?</h3>
                 <div id="rerating-button-group">
                     {this.createbuttons()}
@@ -59,15 +60,16 @@ export default class InterventionRerate extends Component {
                         id="description"
                         hidden={this.state.addDescriptionField}
                         onChange={this.handleFieldChange}
-                    />
-                </div>
-                <div className="center-button">
+                        />
+                <div >
                 <Button
                     color="secondary"
                     variant="contained"
                     onClick={this.createAnxietyRating}
-                >Submit Rating
+                    >Submit Rating
                 </Button>
+                    </div>
+                </div>
                 </div>
             </div>
         )
